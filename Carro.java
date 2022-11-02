e270fbdb98edd0cd7f29ac7c68063d4cc28dd058
allisonvinicius <allisonvinicius63@gmail.com>
2022-11-02 13:04:52 -0400

    feat:  class Carro

---
+++ Carro.java
@@ -0,0 +1,42 @@
+public class Carro {
+
+    //Atributos
+    private String modelo;
+    private Integer ano;
+    private String cor;
+
+
+  public Carro(){
+
+  }
+  public Carro(String modelo, Integer ano, String cor){
+    this.modelo = modelo;
+    this.ano = ano;
+    this.cor = cor;
+
+  }
+
+    public String getModelo() {
+        return modelo;
+    }
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+    public Integer getAno() {
+        return ano;
+    }
+    public void setAno(Integer ano) {
+        this.ano = ano;
+    }
+    public String getCor() {
+        return cor;
+    }
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
+  
+  
+  
+
+   
+}
