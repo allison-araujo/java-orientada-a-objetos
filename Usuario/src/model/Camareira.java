d80cad3f39266affed48be747e2c3c06527849db
allisonvinicius <allisonvinicius63@gmail.com>
2022-11-04 00:16:00 -0400

    feat: include all class

---
+++ Usuario/src/model/Camareira.java
@@ -0,0 +1,10 @@
+package src.model;
+
+public class Camareira extends Pessoa {
+
+    public Camareira(String nome, String telefone, String cpf) {
+        super(nome, telefone, cpf);
+    }
+    
+    
+}
---
+++ Usuario/src/model/Recepcionista.java
@@ -0,0 +1,10 @@
+package src.model;
+
+public class Recepcionista extends Pessoa {
+
+    public Recepcionista(String nome, String telefone, String cpf) {
+        super(nome, telefone, cpf);
+    }
+    
+    
+}
