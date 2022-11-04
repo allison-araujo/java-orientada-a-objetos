8e1f5125f6d8405b4bd0be0102de84ca57593bf1
allisonvinicius <allisonvinicius63@gmail.com>
2022-11-03 23:59:12 -0400

    feat: includes methods in account bnaking

--- src/model/ContaCorrente.java
+++ src/model/ContaCorrente.java
@@ -1,10 +1,36 @@
 package src.model;
 
+import java.util.Date;
+
+import src.utils.DataUtil;
+
 public class ContaCorrente extends ContaBancaria {
 
     public ContaCorrente(String agencia, String conta, Integer digito, Double saldoInicial) {
         //super passa os valores para o pai dele de quem esta herdando
         super(agencia, conta, digito, saldoInicial);
+    }
+
+    @Override
+    public void imprimirExtrato() {
+        System.out.println("********************************************");
+        System.out.println("***************** EXTRATO BANCARIO *********");
+        System.out.println("********************************************");
+        System.out.println();
+        System.out.println("Gerado em: " + DataUtil.convertDateParDataEHora(new Date()));
+        System.out.println();
+
+        for (Movimentacao movimentacao : this.movimentacoes) {
+        System.out.println(movimentacao);   
+        System.out.println();
+
+        }
+        System.out.println("********************************************");
+        System.out.println("********************************************");
+
+
+
+        
     }
 
 
