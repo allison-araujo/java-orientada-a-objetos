280fbc40752aaea7b54cdb54fef09297c15ea54d
allisonvinicius <allisonvinicius63@gmail.com>
2022-11-04 21:23:33 -0400

    feat: include class quartos

---
+++ Usuario/src/model/Quarto.java
@@ -0,0 +1,30 @@
+package src.model;
+
+public class Quarto {
+    private String numero;
+    private Double valorDiaria;
+    private String tipo;
+
+
+
+    public String getNumero() {
+        return numero;
+    }
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+    public Double getValorDiaria() {
+        return valorDiaria;
+    }
+    public void setValorDiaria(Double valorDiaria) {
+        this.valorDiaria = valorDiaria;
+    }
+    public String getTipo() {
+        return tipo;
+    }
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }   
+
+    
+}
